74bfc9a629f9f7dbfaa217549366543054dd408a
Anouk <88028763+AnoukNebbeling@users.noreply.github.com>
2022-05-19 14:39:31 +0200

    Create tiktok.sql

---
+++ marketing_channels/tiktok.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT
+    " "                                     AS account                            
+    ,   date                                
+    ,   "Snapchat"                          AS  medium
+    ,   "Social"                            AS  channel
+    ,   shipping_country
+    ,   campaign_name                       AS  campaign_name
+    ,   impressions                         AS  impressions
+    ,   clicks                              AS  clicks
+    ,   conversions                         AS  conversions
+    ,   conversion_value                    AS  conversion_value
+    ,   spend                               AS  spend                                      
+FROM `vul hier de source in`
