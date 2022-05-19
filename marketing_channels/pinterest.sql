8763a41af247088b2eb36155dbba74d67c9e3bc7
Anouk <88028763+AnoukNebbeling@users.noreply.github.com>
2022-05-19 14:38:30 +0200

    Create pinterest.sql

---
+++ marketing_channels/pinterest.sql
@@ -0,0 +1,14 @@
+
+SELECT DISTINCT
+    " "                                     AS account                            
+    ,   date                                
+    ,   "Pinterest"                         AS  medium
+    ,   "Social"                            AS  channel
+    ,   shipping_country 
+    ,   campaign_name                       AS  campaign_name
+    ,   impressions                         AS  impressions
+    ,   clicks                              AS  clicks
+    ,   conversions                         AS  conversions
+    ,   conversion_value                    AS  conversion_value
+    ,   spend                               AS  spend                                      
+FROM `vul hier de source in`
