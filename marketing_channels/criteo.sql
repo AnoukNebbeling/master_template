2fe861b56dbad4d4634d6b045d3548d9d710b8fb
Anouk <88028763+AnoukNebbeling@users.noreply.github.com>
2022-05-19 14:49:23 +0200

    Create criteo.sql

---
+++ marketing_channels/criteo.sql
@@ -0,0 +1,11 @@
+SELECT 
+date
+, 'Criteo'  AS medium
+, 'Display' AS channel
+, campaign  AS campaign_name
+, impressions
+, clicks
+, cost AS spend
+, conversions
+, conversion_value
+FROM `vul hier de source in`
